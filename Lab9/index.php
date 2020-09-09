6723fd7bb9f6cb7eb5a52c11321555704addeeb3
AlbertoMatuteB <a01704584@itesm.mx>
2020-09-08 20:36:33 -0500

    Entrega Lab8 y avance Lab9

--- Lab9/index.php
+++ Lab9/index.php
@@ -55,21 +55,58 @@ include("_header.html"); ?>
 
     function potencia($total)
     {
-        $lista = "Potencias: ";
-        $lista .= "<ul>";
+
+        $table = "<table>";
         for($i = 0; $i < $total; $i++)
         {
-            $lista .= "<li>". " | " . ($i + 1) ." | ". pow($i+1, 2) . " | " . pow($i+1, 3) . " | " . "</li>";
-            $lista .= "<br>";
+            $table .= "<tr>";
+            $table .= "<td>" . ($i + 1) . "</td>";
+            $table .= "<td>" . pow($i+1, 2) . "</td>";
+            $table .= "<td>" . pow($i+1, 3) . "</td>";
+            $table .= "</tr>";
         }
-        $lista .= "</ul>";
-        return $lista;
+        $table .= "</table>";
+        return $table;
     }
 
-
+    function invertir($num)
+    {
+        $output = strrev($num);
+        return ($output);
+    }
 ?>
 
+
+<?php
+    if(isset($_GET["num"]))
+    {
+        $num=($_GET["num"]);
+    }else
+    {
+        $num = 0;
+    }
+
+    invertir($num);
+?>
 <div class="card-panel teal lighten-2"><?php echo lista($arregloNumeros); ?></div>
 <div class="card-panel red lighten-2"><?php echo potencia(10); ?></div>
+<div class="card-panel blue-grey lighten-3">
+    <form action="index.php" method="get"> 
+        Numero: <input type="text" name="num" value="0">
+        <input class="" type="submit">
+    </form>
+</div>
+
+<div class="container">
+<p>Preguntas</p> 
+
+<p>¿Qué hace la función phpinfo()? Describe y discute 3 datos que llamen tu atención.</p>
+<li><p>La función phpinfo() sirve para mostrar una cierta cantidad de información sobre el estado del PHP, también se puede conocer una representación visual en forma de tabla.</p></li>
+<p>¿Qué cambios tendrías que hacer en la configuración del servidor para que pudiera ser apto en un ambiente de producción?</p>
+<li><p>Los cambios que se realizarían sería instalar una aplicación de enterno con XAMPP ya que con este se puede usar y manejar el servidor.</p></li>
+<p>¿Cómo es que si el código está en un archivo con código html que se despliega del lado del cliente, se ejecuta del lado del servidor? Explica.</p>
+<li><p>Ambos tipos de archibvos se van ejecutando en el servidor, cuando se ejecutan de manera correcta se le envia una respuesta al cliente con lo que se trabajo con HTML y de esa forma se mostrará en la pantalla.</p></li>
+
+</div>
 
 <?php include("_footer.html"); ?>
