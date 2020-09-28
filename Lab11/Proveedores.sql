ef3cbd31b9dbded5464b6bd62f6a718253e9c845
AlbertoMatuteB <a01704584@itesm.mx>
2020-09-28 08:54:56 -0500

    Lab11 entrega

---
+++ Lab11/Proveedores.sql
@@ -0,0 +1,8 @@
+BULK INSERT a1704584.a1704584[Proveedores]
+   FROM 'e:\wwwroot\rcortese\materiales.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
---
+++ Lab11/Proyectos.sql
@@ -0,0 +1,8 @@
+BULK INSERT a1704584.a1704584[Proyectos]
+   FROM 'e:\wwwroot\rcortese\materiales.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
